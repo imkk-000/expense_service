02a795418b1115df7d68605fe8b5d73ad7811bbf
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2020-05-13 19:07:17 +0700

    Rename columns in expenses table

--- SQL/script/2_create_table.sql
+++ SQL/script/2_create_table.sql
@@ -3,7 +3,9 @@ CREATE TABLE [money_app_v1].[dbo].[expenses]
     expense_id INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
     expense_date DATE NOT NULL DEFAULT GETDATE(),
     expense_amount MONEY NOT NULL DEFAULT 0.00,
-    expense_channel SMALLINT NOT NULL DEFAULT NULL,
+    expense_channel_id INT NOT NULL,
+    expense_from_id INT NOT NULL,
+    expense_type_id INT NULL DEFAULT NULL,
     expense_note NVARCHAR(500) NULL DEFAULT NULL,
     expense_user UNIQUEIDENTIFIER NULL DEFAULT NULL,
     expense_ts DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -11,7 +13,7 @@ CREATE TABLE [money_app_v1].[dbo].[expenses]
 
 CREATE TABLE [money_app_v1].[dbo].[expenses_channel]
 (
-    channel_id INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-    channel_description VARCHAR(20) NOT NULL,
-    channel_created_time  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    expense_channel_id INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+    expense_channel_description VARCHAR(20) NOT NULL,
+    expense_channel_created_time  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
