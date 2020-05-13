a5e108f4ae51193c87fe5c1d542e69e78e95f077
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2020-05-13 19:26:29 +0700

    Add new simple insert data

--- SQL/script/3_insert_simple_data.sql
+++ SQL/script/3_insert_simple_data.sql
@@ -1,7 +1,28 @@
 DELETE FROM [money_app_v1].[dbo].[expenses];
 GO
 
+DELETE FROM [money_app_v1].[dbo].[expense_channels];
+GO
+
+DELETE FROM [money_app_v1].[dbo].[expense_froms];
+GO
+
+DELETE FROM [money_app_v1].[dbo].[expense_types];
+GO
+
 INSERT INTO [money_app_v1].[dbo].[expenses]
-(expense_date , expense_amount, expense_channel) VALUES
-(GETDATE()    , 100           , 0),
-(GETDATE()    , 50.25         , 0);
+(expense_date , expense_amount, expense_channel_id, expense_from_id) VALUES
+(GETDATE()    , 100           , 0                 , 0              ),
+(GETDATE()    , 50.25         , 0                 , 0              );
+
+INSERT INTO [money_app_v1].[dbo].[expense_channels]
+(expense_channel_description) VALUES
+('Manual'                   );
+
+INSERT INTO [money_app_v1].[dbo].[expense_froms]
+(expense_from_description) VALUES
+('Not Set'               );
+
+INSERT INTO [money_app_v1].[dbo].[expense_types]
+(expense_type_description) VALUES
+('Not Set'               );
